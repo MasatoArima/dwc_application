7e728f00909ceca02a0661c5947a371570153a07
MasatoArima <afoc.0707@gmail.com>
2021-09-15 22:56:04 +0000

    [finish]課題9a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,12 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    if @book.user_id != current_user.id
+      @book = Book.find(params[:id])
+      impressionist(@book)
+      # impressionist(@book, nil, unique: [:ip_address])
+    end
+    @book = Book.find(params[:id])
     @user = User.find(@book.user_id)
     @newbook = Book.new
     @book_comment = BookComment.new
